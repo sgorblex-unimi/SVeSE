1af3831b6bef3d2bc3aac28c478a8cb82abbbc19
Alessandro Clerici <ale.clericil@gmail.com>
2022-08-27 17:11:19 +0200

    added LICENSE and notices

--- example_data.sql
+++ example_data.sql
@@ -1,3 +1,21 @@
+-- Copyright (C) 2021 Alessandro "Sgorblex" Clerici Lorenzini and Edoardo "Miniman" Della Rossa.
+--
+-- This file is part of SVeSE.
+--
+-- SVeSE is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- SVeSE is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with SVeSE.  If not, see <https://www.gnu.org/licenses/>.
+
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO person VALUES
 -- ssn		birth_date	birth_place	enabled	first_name	last_name	password
